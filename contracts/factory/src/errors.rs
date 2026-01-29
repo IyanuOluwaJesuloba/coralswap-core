b22bcd169161616c4a1b9ff638c1c573bba259e9
Emerson Majormaxx Daniel <125857575+Majormaxx@users.noreply.github.com>
2026-01-29 16:45:00 +0100

    Added factory error types

---
+++ contracts/factory/src/errors.rs
@@ -0,0 +1,16 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum FactoryError {
+    AlreadyInitialized = 1,
+    NotInitialized = 2,
+    PairExists = 3,
+    InvalidSignerCount = 4,
+    InsufficientSignatures = 5,
+    TimelockNotExpired = 6,
+    ProtocolPaused = 7,
+    IdenticalTokens = 8,
+    UpgradeTimelockNotExpired = 9,
+}
