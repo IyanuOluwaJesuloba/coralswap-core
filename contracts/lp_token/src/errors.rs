86ba85a8b18a8ea6d3aecab3070328dff55bf7a6
Emerson Majormaxx Daniel <125857575+Majormaxx@users.noreply.github.com>
2026-02-06 15:00:00 +0100

    Added LP token error types

---
+++ contracts/lp_token/src/errors.rs
@@ -0,0 +1,13 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum LpTokenError {
+    AlreadyInitialized = 200,
+    NotInitialized = 201,
+    Unauthorized = 202,
+    InsufficientBalance = 203,
+    InsufficientAllowance = 204,
+    Overflow = 205,
+}
