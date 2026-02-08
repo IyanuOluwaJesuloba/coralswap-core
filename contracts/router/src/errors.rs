4322894fa954d0038260f85ec373bc3b07847960
Emerson Majormaxx Daniel <125857575+Majormaxx@users.noreply.github.com>
2026-02-08 16:15:00 +0100

    Added router error types

---
+++ contracts/router/src/errors.rs
@@ -0,0 +1,14 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum RouterError {
+    Expired = 300,
+    InsufficientOutputAmount = 301,
+    ExcessiveInputAmount = 302,
+    InvalidPath = 303,
+    PairNotFound = 304,
+    IdenticalTokens = 305,
+    ZeroAmount = 306,
+}
