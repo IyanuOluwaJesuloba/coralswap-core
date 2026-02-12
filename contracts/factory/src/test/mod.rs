79b41c8c1ad7d1418044ad15a0e0fed3dcad8751
Emerson Majormaxx Daniel <125857575+Majormaxx@users.noreply.github.com>
2026-02-12 16:00:00 +0100

    Added factory unit test module

---
+++ contracts/factory/src/test/mod.rs
@@ -0,0 +1,32 @@
+#![cfg(test)]
+
+use soroban_sdk::Env;
+
+mod factory_tests {
+    use super::*;
+
+    #[test]
+    fn test_placeholder_initialization() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_create_pair() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_duplicate_pair_rejected() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_pause_requires_multisig() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_circuit_breaker() {
+        let _env = Env::default();
+    }
+}
