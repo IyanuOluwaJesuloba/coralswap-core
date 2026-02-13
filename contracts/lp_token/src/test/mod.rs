9849ca1f03ced8159077cad5dd6e7737280bc501
Emerson Majormaxx Daniel <125857575+Majormaxx@users.noreply.github.com>
2026-02-13 14:45:00 +0100

    Added LP token unit test module

---
+++ contracts/lp_token/src/test/mod.rs
@@ -0,0 +1,32 @@
+#![cfg(test)]
+
+use soroban_sdk::Env;
+
+mod lp_token_tests {
+    use super::*;
+
+    #[test]
+    fn test_placeholder_mint_by_pair() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_burn_by_pair() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_transfer() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_unauthorized_mint_rejected() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_allowance_expiry() {
+        let _env = Env::default();
+    }
+}
