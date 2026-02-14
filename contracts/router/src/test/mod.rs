4fc74db467671847526255460e8ee186a843c435
Emerson Majormaxx Daniel <125857575+Majormaxx@users.noreply.github.com>
2026-02-14 10:00:00 +0100

    Added router unit test module

---
+++ contracts/router/src/test/mod.rs
@@ -0,0 +1,32 @@
+#![cfg(test)]
+
+use soroban_sdk::Env;
+
+mod router_tests {
+    use super::*;
+
+    #[test]
+    fn test_placeholder_swap_exact_in() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_swap_exact_out() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_expired_deadline_rejected() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_add_liquidity() {
+        let _env = Env::default();
+    }
+
+    #[test]
+    fn test_placeholder_remove_liquidity() {
+        let _env = Env::default();
+    }
+}
